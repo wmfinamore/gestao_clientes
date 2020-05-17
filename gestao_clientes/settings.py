9f6aae2bd91dfe595254cc0df29a517c896ae6a7
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-05-16 22:31:18 -0300

    Ajuste no setting para deploy

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# default_dburl = 'sqlite3:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-# DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+default_dburl = 'sqlite3:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestao_clientes',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gestao_clientes',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
