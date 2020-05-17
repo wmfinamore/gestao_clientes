887e7772559e3bc77d6841513490ba2593ef7825
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-05-16 22:29:53 -0300

    Personalização dos templates com bootstrap

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'clientes',
     'home',
 ]
@@ -129,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 STATICFILES_DIRS = [
-    'static',
+    'statics',
 ]
 
 MEDIA_URL = '/media/'
