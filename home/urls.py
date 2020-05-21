1e1296b7af80b90053c9a15f895745799b119736
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-05-20 23:55:43 -0300

    Commit inicial do curso avançado de Django. Implementado pequeno exemplo de uso de TemplateView

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import home, logout_user
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('logout/', logout_user, name="logout"),
+    path('home2/', TemplateView.as_view(template_name='home2.html'), name="home2"),
 
 ]
