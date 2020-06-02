8353a4fb4532f6257b1bd6db310980db71d04399
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-06-01 21:28:32 -0300

    Implementado exemplo de utilização de list_filter para personalização do Django Admin

--- clientes/admin.py
+++ clientes/admin.py
@@ -15,9 +15,13 @@ class PersonAdmin(admin.ModelAdmin):
     # fields = (('doc', 'first_name'), 'last_name', ('age', 'salary'), 'bio', 'photo')
     # exclude = ('bio', )
     list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'photo')
+    list_filter = ('age', 'salary')
+
+class VendaAdmin(admin.ModelAdmin):
+    list_filter = ('pessoa__doc', 'desconto')
 
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
-admin.site.register(Venda)
+admin.site.register(Venda, VendaAdmin)
 admin.site.register(Produto)
