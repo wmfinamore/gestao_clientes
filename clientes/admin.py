535338fb2019a6c93be1635c2133bc4546181367
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-06-01 21:18:42 -0300

    Implementado exemplo de customização de campos no Django Admin

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
+class PersonAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (
+            'Dados Pessoais', {'fields': ('first_name', 'last_name', 'doc')}
+        ),
+        (
+            'Dados Complementares', {
+                'classes': ('collapse', ),
+                'fields': ('age', 'salary', 'photo', 'bio')}
+        )
+    )
+    # fields = (('doc', 'first_name'), 'last_name', ('age', 'salary'), 'bio', 'photo')
+    # exclude = ('bio', )
+    list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'photo')
 
-admin.site.register(Person)
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
