8d8fbbea9384f2788aa0e87c985367aa95ea8a1f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-06-04 22:17:26 -0300

    Iniciada a implementação de exemplo de utilização funções de aggregate e calculos automáticos

--- vendas/admin.py
+++ vendas/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from .actions import nfe_nao_emitida, nfe_emitida
-from .models import Venda, ItensDoPedido
+from .models import Venda, ItemDoPedido
 
+class ItemPedidoInLine(admin.TabularInline):
+    model = ItemDoPedido
 
 class VendaAdmin(admin.ModelAdmin):
-    readonly_fields = ('desconto',)
+    # readonly_fields = ('desconto',)
     list_filter = ('pessoa__doc', 'desconto')
-    list_display = ('id', 'pessoa', 'nfe_emitida',)
+    list_display = ('numero', 'pessoa', 'valor', 'nfe_emitida',)
     autocomplete_fields = ('pessoa',)
     search_fields = ('id', 'pessoa__first_name', 'pessoa__doc__num_doc')
     actions = [nfe_emitida, nfe_nao_emitida]
+    inlines = [ItemPedidoInLine]
 
 
 admin.site.register(Venda, VendaAdmin)
-admin.site.register(ItensDoPedido)
+admin.site.register(ItemDoPedido)
--- vendas/models.py
+++ vendas/models.py
@@ -1,32 +1,33 @@
 from django.db import models
+from django.db.models import Sum, F, FloatField, Max
 from clientes.models import Person
 from produtos.models import Produto
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
 
 class Venda(models.Model):
     numero = models.CharField(max_length=7)
     valor = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    desconto = models.DecimalField(max_digits=5, decimal_places=2)
-    impostos = models.DecimalField(max_digits=5, decimal_places=2)
+    desconto = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    impostos = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
     # produtos = models.ManyToManyField(Produto, blank=True)
     nfe_emitida = models.BooleanField(default=False)
 
-    # def get_total(self):
-    #     tot = 0
-    #     for produto in self.produtos.all():
-    #         tot += produto.preco
-    #
-    #     tot = (tot - self.desconto) - self.desconto
-    #     return tot
+    def calcular_total(self):
+        tot = self.itemdopedido_set.all().aggregate(
+            tot_ped=Sum((F('quantidade') * F('produto__preco')) * (1- F('desconto')), output_field=FloatField())
+        )['tot_ped']
+        tot = float(tot) - float(self.impostos) - float(self.desconto)
+        self.valor = tot
+        Venda.objects.filter(id=self.id).update(valor=tot)
 
     def __str__(self):
         return self.numero
 
 
-class ItensDoPedido(models.Model):
+class ItemDoPedido(models.Model):
     venda = models.ForeignKey(Venda, on_delete=models.PROTECT)
     produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
     quantidade = models.FloatField()
@@ -34,3 +35,13 @@ class ItensDoPedido(models.Model):
 
     def __str__(self):
         return self.venda.numero + '-' + self.produto.descricao
+
+
+@receiver(post_save, sender=ItemDoPedido)
+def update_vendas_total(sender, instance, **kwargs):
+    instance.venda.calcular_total()
+
+
+@receiver(post_save, sender=Venda)
+def update_vendas_total2(sender, instance, **kwargs):
+    instance.calcular_total()
