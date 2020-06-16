bc06a7756fe7bd39f0a9951d80cc197a13135c86
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-06-16 20:57:41 -0300

    Implememtado template para manipulação de erro 500

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -25,11 +25,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$vb^q+=iwv!eq$(b_)t71qm99hz@&kx&hb@d_*i+9#v4*66tta'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = ['127.0.0.1']
+
+# ADMINS = [('Willian', 'teste@gmail.com'),]
 
 # Application definition
 
