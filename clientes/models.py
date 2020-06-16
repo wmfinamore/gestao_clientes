4f80f8e4e6dc2d28eccd078b48902530f983423a
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-06-15 22:08:34 -0300

    Implementado exemplo de envio de e-mail quando um cliente novo é cadastrado

--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.core.mail import send_mail
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
+from django.template.loader import render_to_string
 
 
 class Documento(models.Model):
@@ -28,6 +30,21 @@ class Person(models.Model):
     @property
     def nome_completo(self):
         return self.first_name + ' ' + self.last_name
+
+    def save(self, *args, **kwargs):
+        super(Person, self).save(*args, **kwargs)
+
+        data = {'cliente': self.first_name}
+        plain_text = render_to_string('clientes/emails/novo_cliente.txt', data)
+        html_email = render_to_string('clientes/emails/novo_cliente.html', data)
+        send_mail(
+            'Novo cliente cadastrado',
+            plain_text,
+            'teste@gmail.com',
+            ['teste@hotmail.com'],
+            html_message=html_email,
+            fail_silently=False,
+        )
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
